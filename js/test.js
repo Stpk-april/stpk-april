9720bd8db3e693500291b63f3e04f7a9a6f34194
Sei <stpkkik77@gmail.com>
2015-02-12 23:18:09 +0900

    testing

---
+++ js/test.js
@@ -0,0 +1,8 @@
+﻿
+$(function(){
+
+$.getJSON("http://cors.io/spreadsheets.google.com/feeds/list/1DbyWSpmUlfygdhQe6q6HSvDukGjIYwKC2WSznRzdQpU/od6/public/values?alt=json", function(data) {
+  //first row "title" column
+  console.log(data.feed.entry[0]['gsx$title']['$t']);
+});
+})
